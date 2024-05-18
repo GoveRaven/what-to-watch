e1afa97650fd123d00244f1344a21ee27df81400
Даня <daniilevgen79@gmail.com>
2024-05-18 22:02:57 +0300

    обновил app, добавив маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,42 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoutes, AuthorizationStatus } from './const';
+import { AddReview } from '../../pages/add-review';
 import { Main, MainProps } from '../../pages/main';
+import { MoviePage } from '../../pages/movie';
+import { MyList } from '../../pages/my-list';
+import { PageNotFound } from '../../pages/page-not-found';
+import { Player } from '../../pages/player';
+import { SignIn } from '../../pages/sign-in';
+import { PrivateRoute } from './private-route';
 
-type AppProps = MainProps
+type AppProps = MainProps;
 
 function App({ title, genre, releaseDate }: AppProps): JSX.Element {
-  return <Main title={title} genre={genre} releaseDate={releaseDate} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoutes.Main}
+          element={
+            <Main title={title} genre={genre} releaseDate={releaseDate} />
+          }
+        />
+        <Route path={AppRoutes.SignIn} element={<SignIn />} />
+        <Route
+          path={AppRoutes.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoutes.Film} element={<MoviePage />} />
+        <Route path={AppRoutes.AddReview} element={<AddReview />} />
+        <Route path={AppRoutes.Player} element={<Player />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

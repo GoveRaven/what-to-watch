3e89160e43c9f7d9c78679e2dc11ffb51c6e5d3b
Даня <daniilevgen79@gmail.com>
2024-05-25 20:28:43 +0300

    добавил моки с фильмами

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,200 @@
+import { TFilm } from '../types/films';
+
+export const films: TFilm[] = [
+  {
+    name: 'Once Upon a Time in America',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Once_Upon_a_Time_in_America.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/Once_Upon_a_Time_in_America.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/ones_upon_a_time_in_america.jpg',
+    backgroundColor: '#CBAC79',
+    description:
+      'A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life.',
+    rating: 9.9,
+    scoresCount: 276395,
+    director: 'Sergio Leone',
+    starring: ['Robert De Niro', 'James Woods', 'Elizabeth McGovern'],
+    runTime: 229,
+    genre: 'Crime',
+    released: 1984,
+    id: 1,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/dog.mp4',
+  },
+  {
+    name: 'Dardjeeling Limited',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Dardjeeling_Limited.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/dardjeeling_limited.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/Dardjeeling_Limited.jpg',
+    backgroundColor: '#AD9F8B',
+    description:
+      'A year after their father\'s funeral, three brothers travel across India by train in an attempt to bond with each other.',
+    rating: 3.6,
+    scoresCount: 165106,
+    director: 'Wes Anderson',
+    starring: ['Owen Wilson', 'Adrien Brody', 'Jason Schwartzman'],
+    runTime: 91,
+    genre: 'Adventure',
+    released: 2007,
+    id: 2,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/traffic.mp4',
+  },
+  {
+    name: 'Orlando',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Orlando.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/orlando.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/Orlando.jpg',
+    backgroundColor: '#D8D3BD',
+    description:
+      'Young nobleman Orlando is commanded by Queen Elizabeth I to stay forever young. Miraculously, he does just that. The film follows him as he moves through several centuries of British history, experiencing a variety of lives and relationships along the way, and even changing sex.',
+    rating: 2.6,
+    scoresCount: 12292,
+    director: 'Sally Potter',
+    starring: ['Tilda Swinton', 'Billy Zane', 'Quentin Crisp'],
+    runTime: 94,
+    genre: 'Drama',
+    released: 1992,
+    id: 3,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/dog.mp4',
+  },
+  {
+    name: 'Gangs of new york',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Gangs_of_New_York_Poster.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/gangs_of_new_york.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/gangs_of_new_york.jpg',
+    backgroundColor: '#A6B7AC',
+    description:
+      'In 1862, Amsterdam Vallon returns to the Five Points area of New York City seeking revenge against Bill the Butcher, his father\'s killer.',
+    rating: 8.8,
+    scoresCount: 370881,
+    director: 'Martin Scorsese',
+    starring: ['Leonardo DiCaprio', 'Cameron Diaz', 'Daniel Day-Lewis'],
+    runTime: 167,
+    genre: 'Crime',
+    released: 2002,
+    id: 4,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/traffic.mp4',
+  },
+  {
+    name: 'War of the Worlds',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/War_of_the_Worlds.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/war-of-the-worlds.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/War_of_the_Worlds.jpg',
+    backgroundColor: '#9B7E61',
+    description:
+      'As Earth is invaded by alien tripod fighting machines, one family fights for survival.',
+    rating: 9.3,
+    scoresCount: 386834,
+    director: 'Steven Spielberg',
+    starring: ['Tom Cruise', 'Dakota Fanning', 'Tim Robbins'],
+    runTime: 116,
+    genre: 'Adventure',
+    released: 2005,
+    id: 5,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/traffic.mp4',
+  },
+  {
+    name: 'Shutter Island',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Shutter_Island.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/shutter-island.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/Shutter_Island.jpg',
+    backgroundColor: '#977461',
+    description:
+      'In 1954, a U.S. Marshal investigates the disappearance of a murderer, who escaped from a hospital for the criminally insane.',
+    rating: 4.1,
+    scoresCount: 1002557,
+    director: 'Martin Scorsese',
+    starring: ['Leonardo DiCaprio', 'Emily Mortimer', 'Mark Ruffalo'],
+    runTime: 138,
+    genre: 'Thriller',
+    released: 2010,
+    id: 6,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/traffic.mp4',
+  },
+  {
+    name: 'Aviator',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/Aviator.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/aviator.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/Aviator.jpg',
+    backgroundColor: '#D6CDAF',
+    description:
+      'A biopic depicting the early years of legendary Director and aviator Howard Hughes\' career from the late 1920s to the mid 1940s.',
+    rating: 9.8,
+    scoresCount: 307174,
+    director: 'Martin Scorsese',
+    starring: ['Leonardo DiCaprio', 'Cate Blanchett', 'Kate Beckinsale'],
+    runTime: 170,
+    genre: 'Drama',
+    released: 2014,
+    id: 7,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bubbles.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/dog.mp4',
+  },
+  {
+    name: 'Beach',
+    posterImage:
+      'https://10.react.htmlacademy.pro/static/film/poster/beach.jpg',
+    previewImage:
+      'https://10.react.htmlacademy.pro/static/film/preview/beach.jpg',
+    backgroundImage:
+      'https://10.react.htmlacademy.pro/static/film/background/beach.jpg',
+    backgroundColor: '#EBC996',
+    description:
+      'Vicenarian Richard travels to Thailand and finds himself in possession of a strange map. Rumours state that it leads to a solitary beach paradise, a tropical bliss. Excited and intrigued, he sets out to find it.',
+    rating: 3.3,
+    scoresCount: 207824,
+    director: 'Danny Boyle',
+    starring: [
+      'Leonardo DiCaprio',
+      'Daniel York',
+      'Patcharawan Patarakijjanon',
+    ],
+    runTime: 119,
+    genre: 'Adventure',
+    released: 2000,
+    id: 8,
+    isFavorite: false,
+    videoLink: 'https://10.react.htmlacademy.pro/static/film/video/bike.mp4',
+    previewVideoLink:
+      'https://10.react.htmlacademy.pro/static/film/video/dog.mp4',
+  },
+];

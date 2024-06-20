48a54750d12bd936bcc0285f94bc0c4c28f86984
Даня <daniilevgen79@gmail.com>
2024-06-21 00:03:21 +0300

    вынес колонку с ревью в комноненту

---
+++ project/src/components/tabs/reviews column.tsx
@@ -0,0 +1,49 @@
+import { TReview } from '../../types/reviews';
+
+type TReviewsProps = {
+  reviews: TReview[];
+};
+
+function getDate(date: string) {
+  const fullDate = new Date(date);
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return `${
+    months[fullDate.getMonth()]
+  } ${fullDate.getDate()} ,${fullDate.getFullYear()}`;
+}
+
+export function ReviewColumn({ reviews }: TReviewsProps) {
+  return (
+    <div className="film-card__reviews-col">
+      {reviews.map((review) => (
+        <div className="review" key={review.id}>
+          <blockquote className="review__quote">
+            <p className="review__text">{review.comment}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">{review.user.name}</cite>
+              <time className="review__date" dateTime={review.date}>
+                {getDate(review.date)}
+              </time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">{review.rating}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- project/src/components/tabs/reviews.tsx
+++ project/src/components/tabs/reviews.tsx
@@ -1,72 +1,18 @@
 import { TReview } from '../../types/reviews';
+import { ReviewColumn } from './reviews column';
 
 type TReviewsProps = {
   reviews: TReview[];
 };
 
-function getDate(date: string) {
-  const fullDate = new Date(date);
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  return `${
-    months[fullDate.getMonth()]
-  } ${fullDate.getDate()} ,${fullDate.getFullYear()}`;
-}
-
 export function Reviews({ reviews }: TReviewsProps) {
   const halfOfReviews = Math.ceil(reviews.length / 2);
-  const reviews1 = reviews.slice(0, halfOfReviews);
-  const reviews2 = reviews.slice(halfOfReviews);
+  const firstReviewsHalf = reviews.slice(0, halfOfReviews);
+  const secondReviewsHalf = reviews.slice(halfOfReviews);
   return (
     <div className="film-card__reviews film-card__row">
-      <div className="film-card__reviews-col">
-        {reviews1.map((review) => (
-          <div className="review" key={review.id}>
-            <blockquote className="review__quote">
-              <p className="review__text">{review.comment}</p>
-
-              <footer className="review__details">
-                <cite className="review__author">{review.user.name}</cite>
-                <time className="review__date" dateTime={review.date}>
-                  {getDate(review.date)}
-                </time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">{review.rating}</div>
-          </div>
-        ))}
-      </div>
-      <div className="film-card__reviews-col">
-        {reviews2.map((review) => (
-          <div className="review" key={review.id}>
-            <blockquote className="review__quote">
-              <p className="review__text">{review.comment}</p>
-
-              <footer className="review__details">
-                <cite className="review__author">{review.user.name}</cite>
-                <time className="review__date" dateTime={review.date}>
-                  {getDate(review.date)}
-                </time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">{review.rating}</div>
-          </div>
-        ))}
-      </div>
+      <ReviewColumn reviews={firstReviewsHalf}/>
+      <ReviewColumn reviews={secondReviewsHalf}/>
     </div>
   );
 }

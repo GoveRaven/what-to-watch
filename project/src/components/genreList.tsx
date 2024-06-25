133777ba7933e93ec297c02648785adf62ccc5e7
Даня <daniilevgen79@gmail.com>
2024-06-25 22:44:30 +0300

    добавил компоненту со списком жанров

---
+++ project/src/components/genreList.tsx
@@ -0,0 +1,37 @@
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { changeGenre, FilterFilmsByGenre } from '../store/actions';
+import { TFilm } from '../types/films';
+
+type TGenresList = {
+  films: TFilm[];
+};
+
+export function GenresList({ films }: TGenresList) {
+  const genres = ['All genres'];
+  const dispatch = useAppDispatch();
+  const activeGenre = useAppSelector((state) => state.genre);
+
+  films.map((film) => genres.includes(film.genre) || genres.push(film.genre));
+  return (
+    <ul className="catalog__genres-list">
+      {genres.map((genre) => (
+        <li
+          className={`catalog__genres-item${
+            activeGenre === genre ? ' catalog__genres-item--active' : ''
+          }`}
+          key={genre}
+        >
+          <button
+            className="catalog__genres-link"
+            onClick={() => {
+              dispatch(changeGenre(genre));
+              dispatch(FilterFilmsByGenre());
+            }}
+          >
+            {genre}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}

f0da64b7d8bfa7a6821fc9fe0c49ad0f86cc008a
Даня <daniilevgen79@gmail.com>
2024-07-05 17:06:42 +0300

    убрал моковые фильмы добавил 2 поля хранилищу добавил редьюсеру действия

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,12 +1,28 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { changeGenre, actualizeFilmsList } from './actions';
-import { films } from '../mocks/films';
+import {
+  changeGenre,
+  actualizeFilmsList,
+  loadFilms,
+  setFilmsLoadingStatus,
+} from './actions';
 import { DEFAULT_GENRE } from '../consts/films';
+import { TFilm } from '../types/films';
+import { AuthorizationStatus } from '../consts/authhorization-status';
 
-const initialState = {
+type TInitialState = {
+  genre: string;
+  films: TFilm[];
+  defaultFilmsList: TFilm[];
+  isFilmsLoading: boolean;
+  AuthStatus: AuthorizationStatus;
+};
+
+const initialState: TInitialState = {
   genre: DEFAULT_GENRE,
-  films: films,
-  defaultFilmsList: films,
+  films: [],
+  defaultFilmsList: [],
+  isFilmsLoading: true,
+  AuthStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -16,11 +32,18 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(actualizeFilmsList, (state) => {
       if (state.genre === DEFAULT_GENRE) {
-        state.films = initialState.films;
+        state.films = state.defaultFilmsList;
       } else {
-        state.films = initialState.films.filter(
+        state.films = state.defaultFilmsList.filter(
           (film) => film.genre === state.genre
         );
       }
+    })
+    .addCase(loadFilms, (state, action) => {
+      state.films = action.payload;
+      state.defaultFilmsList = action.payload;
+    })
+    .addCase(setFilmsLoadingStatus, (state, action) => {
+      state.isFilmsLoading = action.payload;
     });
 });

fddd894b5fb239ab76f202ff30101ef2020e1089
Даня <daniilevgen79@gmail.com>
2024-07-06 15:20:50 +0300

    сделал отступ от действий фильмов и действия сервера

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -3,6 +3,7 @@ import { TFilm } from '../types/films';
 
 export const changeGenre = createAction<string>('films/changeGenre');
 export const actualizeFilmsList = createAction('films/actualizeFilmsList');
+
 export const loadFilms = createAction<TFilm[]>('data/loadFilms');
 export const setFilmsLoadingStatus = createAction<boolean>(
   'data/setFilmsLoadingStatus'

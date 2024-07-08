e28001b2dbf18978255933d45911b1d028689195
Даня <daniilevgen79@gmail.com>
2024-07-08 23:23:58 +0300

    добавил компоненту с блоком пользователя

---
+++ project/src/components/user-block.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { AuthorizationStatus } from '../consts/authhorization-status';
+import { useAppSelector } from '../hooks';
+import { AppRoute } from '../consts/routes';
+import { store } from '../store';
+import { authLogout } from '../store/api-action';
+
+export function UserBlock(): JSX.Element {
+  const authStatus = useAppSelector((state) => state.authStatus);
+
+  function cliclHandler() {
+    store.dispatch(authLogout());
+  }
+
+  if (authStatus === AuthorizationStatus.Auth) {
+    return (
+      <ul className="user-block">
+        <li className="user-block__item">
+          <div className="user-block__avatar">
+            <img
+              src="img/avatar.jpg"
+              alt="User avatar"
+              width="63"
+              height="63"
+            />
+          </div>
+        </li>
+        <li className="user-block__item">
+          <button className="user-block__link" onClick={cliclHandler}>
+            Sign out
+          </button>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <div className="user-block">
+        <Link to={AppRoute.SignIn} className="user-block__link">
+          Sign in
+        </Link>
+      </div>
+    );
+  }
+}

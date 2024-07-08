21f33c286cdbb54355a99847bbcc7e7e9eb5cd4f
Даня <daniilevgen79@gmail.com>
2024-07-08 23:31:16 +0300

    добавил код для авторизации с запросами на сервер

--- project/src/store/api-action.ts
+++ project/src/store/api-action.ts
@@ -2,8 +2,16 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { TAppDispatch, TState } from '../types/store';
 import { AxiosInstance } from 'axios';
 import { TFilm } from '../types/films';
-import { APIRoute } from '../consts/routes';
-import { setFilms, setFilmsLoadingStatus } from './actions';
+import { APIRoute, AppRoute } from '../consts/routes';
+import {
+  redirectToRoute,
+  setAuthStatus,
+  setFilms,
+  setFilmsLoadingStatus,
+} from './actions';
+import { TUser } from '../types/user';
+import { AuthorizationStatus } from '../consts/authhorization-status';
+import { dropToken, saveToken } from '../services/token';
 
 export const fetchFilmAction = createAsyncThunk<
   void,
@@ -18,4 +26,52 @@ export const fetchFilmAction = createAsyncThunk<
   const { data } = await api.get<TFilm[]>(APIRoute.Films);
   dispatch(setFilms(data));
   dispatch(setFilmsLoadingStatus(false));
+});
+
+export const checkAuth = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: TAppDispatch;
+    state: TState;
+    extra: AxiosInstance;
+  }
+>('user/checkAuth', async (_arg, { dispatch, extra: api }) => {
+  try {
+    await api.get<TUser>(APIRoute.Login);
+    dispatch(setAuthStatus(AuthorizationStatus.Auth));
+  } catch {
+    dispatch(setAuthStatus(AuthorizationStatus.NoAuth));
+  }
+});
+
+export const authLogin = createAsyncThunk<
+  void,
+  { email: string; password: string },
+  {
+    dispatch: TAppDispatch;
+    state: TState;
+    extra: AxiosInstance;
+  }
+>('user/authLogin', async (data, { dispatch, extra: api }) => {
+  const {
+    data: { token },
+  } = await api.post<TUser>(APIRoute.Login, data);
+  saveToken(token);
+  dispatch(setAuthStatus(AuthorizationStatus.Auth));
+  dispatch(redirectToRoute(AppRoute.Main));
+});
+
+export const authLogout = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: TAppDispatch;
+    state: TState;
+    extra: AxiosInstance;
+  }
+>('user/authLogout', async (_arg, { dispatch, extra: api }) => {
+  await api.delete<TUser>(APIRoute.logout);
+  dropToken();
+  dispatch(setAuthStatus(AuthorizationStatus.NoAuth));
 });

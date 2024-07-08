f494ee5f12a7cdc35ff3732dc896c08b526d5d79
Даня <daniilevgen79@gmail.com>
2024-07-08 23:30:43 +0300

    добавил действия

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,5 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { TFilm } from '../types/films';
+import { AuthorizationStatus } from '../consts/authhorization-status';
+import { AppRoute } from '../consts/routes';
 
 export const changeGenre = createAction<string>('films/changeGenre');
 export const actualizeFilmsList = createAction('films/actualizeFilmsList');
@@ -8,3 +10,8 @@ export const setFilms = createAction<TFilm[]>('data/setFilms');
 export const setFilmsLoadingStatus = createAction<boolean>(
   'data/setFilmsLoadingStatus'
 );
+
+export const setAuthStatus =
+  createAction<AuthorizationStatus>('user/setAuthStatus');
+
+export const redirectToRoute = createAction<AppRoute>('route/redirectToRoute');

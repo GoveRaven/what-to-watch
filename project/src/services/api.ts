74c893602d5568722d0450a26aaee9b7b4f4eae4
Даня <daniilevgen79@gmail.com>
2024-07-08 23:32:27 +0300

    добавил interceptors

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,12 +1,30 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://10.react.htmlacademy.pro/wtw';
 const REQUEST_TIMEOUT = 5000;
 
 export const createApi = (): AxiosInstance => {
   const api = axios.create({
-    baseURL: BACKEND_URL ,
+    baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+    if (token) {
+      config.headers['X-Token'] = token;
+    }
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      // TODO: заменить alert на компоненту с ошибкой
+      // alert(error.message);
+      throw error;
+    }
+  );
   return api;
 };

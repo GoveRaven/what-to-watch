43b4fe76e61616d6e5bff69a5f4c43e3ba40a2eb
Даня <daniilevgen79@gmail.com>
2024-07-09 14:09:17 +0300

    переместил пробел

--- project/src/components/logo.tsx
+++ project/src/components/logo.tsx
@@ -10,7 +10,7 @@ export function Logo({ needClass }: TLogo): JSX.Element {
     <div className="logo">
       <Link
         to={AppRoute.Main}
-        className={`logo__link ${needClass ? 'logo__link--light' : ''}`}
+        className={`logo__link${needClass ? ' logo__link--light' : ''}`}
       >
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>

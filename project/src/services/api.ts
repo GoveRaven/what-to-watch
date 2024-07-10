3612be24d2d1e27963d86a0a8a235101da4b1982
Даня <daniilevgen79@gmail.com>
2024-07-10 18:23:49 +0300

    убрал вывод ошибки не авторизации

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -18,7 +18,6 @@ type DetailMessageType = {
 
 const StatusCodeMapping: Record<number, boolean> = {
   [StatusCodes.BAD_REQUEST]: true,
-  [StatusCodes.UNAUTHORIZED]: true,
   [StatusCodes.NOT_FOUND]: true,
 };
 

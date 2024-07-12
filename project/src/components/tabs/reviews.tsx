b543ac27c961a082f0a491626eece07022ed678b
Даня <daniilevgen79@gmail.com>
2024-07-12 14:31:47 +0300

    заменил пропс на данные с хранилища

--- project/src/components/tabs/reviews.tsx
+++ project/src/components/tabs/reviews.tsx
@@ -1,3 +1,4 @@
+import { useAppSelector } from '../../hooks';
 import { TReview } from '../../types/reviews';
 import { ReviewColumn } from './reviews-column';
 
@@ -5,14 +6,16 @@ export type TReviewsProps = {
   reviews: TReview[];
 };
 
-export function Reviews({ reviews }: TReviewsProps) {
+export function Reviews() {
+  const reviews = useAppSelector((state) => state.filmComments);
+
   const halfOfReviews = Math.ceil(reviews.length / 2);
   const firstReviewsHalf = reviews.slice(0, halfOfReviews);
   const secondReviewsHalf = reviews.slice(halfOfReviews);
   return (
     <div className="film-card__reviews film-card__row">
-      <ReviewColumn reviews={firstReviewsHalf}/>
-      <ReviewColumn reviews={secondReviewsHalf}/>
+      <ReviewColumn reviews={firstReviewsHalf} />
+      <ReviewColumn reviews={secondReviewsHalf} />
     </div>
   );
 }

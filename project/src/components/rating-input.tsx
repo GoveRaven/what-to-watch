95f45cd33839b9cf569cba9352b5bc46ced26158
Даня <daniilevgen79@gmail.com>
2024-07-13 12:10:09 +0300

    добавил компоненту radio-кнопок и добавил на страницу с ревью

---
+++ project/src/components/rating-input.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent } from 'react';
+
+type TRatingInput = {
+  rating: string;
+  ratingHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function RatingInput({ rating, ratingHandler }: TRatingInput) {
+  const rates = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+  return (
+    <div className="rating__stars">
+      {rates.map((rate) => (
+        <>
+          <input
+            className="rating__input"
+            id={`star-${rate}`}
+            type="radio"
+            name="rating"
+            value={rate}
+            onChange={ratingHandler}
+            checked={Number(rating) === rate}
+            key={rate}
+          />
+          <label className="rating__label" htmlFor={`star-${rate}`}>
+            Rating {rate}
+          </label>
+        </>
+      ))}
+    </div>
+  );
+}

bdf4056f745ebb97696946d5fa41299163736a5f
Даня <daniilevgen79@gmail.com>
2024-07-13 17:04:27 +0300

    переимменовал переменную

--- project/src/components/tabs/reviews.tsx
+++ project/src/components/tabs/reviews.tsx
@@ -2,11 +2,11 @@ import { useAppSelector } from '../../hooks';
 import { ReviewColumn } from './reviews-column';
 
 export function Reviews() {
-  const reviews = useAppSelector((state) => state.filmComments);
+  const chosenFilmsComments = useAppSelector((state) => state.filmComments);
 
-  const halfOfReviews = Math.ceil(reviews.length / 2);
-  const firstReviewsHalf = reviews.slice(0, halfOfReviews);
-  const secondReviewsHalf = reviews.slice(halfOfReviews);
+  const halfOfReviews = Math.ceil(chosenFilmsComments.length / 2);
+  const firstReviewsHalf = chosenFilmsComments.slice(0, halfOfReviews);
+  const secondReviewsHalf = chosenFilmsComments.slice(halfOfReviews);
   return (
     <div className="film-card__reviews film-card__row">
       <ReviewColumn reviews={firstReviewsHalf} />

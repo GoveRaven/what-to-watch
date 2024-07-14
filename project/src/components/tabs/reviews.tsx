9d7f2ee735e70788ebe1fa8eb489e99ea6a2d55c
Даня <daniilevgen79@gmail.com>
2024-07-14 15:12:27 +0300

    переименовал переменную

--- project/src/components/tabs/reviews.tsx
+++ project/src/components/tabs/reviews.tsx
@@ -4,20 +4,20 @@ import { Loader } from '../loader';
 import { ReviewColumn } from './reviews-column';
 
 export function Reviews() {
-  const chosenFilmsComments = useAppSelector((state) => state.filmComments);
+  const chosenFilmComments = useAppSelector((state) => state.filmComments);
   const isFilmCommentLoading = useAppSelector(
     (state) => state.isFilmCommentLoading
   );
 
-  if (isFilmCommentLoading && !chosenFilmsComments) {
+  if (isFilmCommentLoading && !chosenFilmComments) {
     return <Loader />;
-  } else if (!isFilmCommentLoading && !chosenFilmsComments) {
+  } else if (!isFilmCommentLoading && !chosenFilmComments) {
     return <NotFound />;
   }
 
-  const halfOfReviews = Math.ceil(chosenFilmsComments.length / 2);
-  const firstReviewsHalf = chosenFilmsComments.slice(0, halfOfReviews);
-  const secondReviewsHalf = chosenFilmsComments.slice(halfOfReviews);
+  const halfOfReviews = Math.ceil(chosenFilmComments.length / 2);
+  const firstReviewsHalf = chosenFilmComments.slice(0, halfOfReviews);
+  const secondReviewsHalf = chosenFilmComments.slice(halfOfReviews);
   return (
     <div className="film-card__reviews film-card__row">
       <ReviewColumn reviews={firstReviewsHalf} />

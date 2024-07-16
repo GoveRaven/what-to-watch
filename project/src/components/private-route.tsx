71e4862fe579e6c730d53b72c434e08c3d77ced7
Даня <daniilevgen79@gmail.com>
2024-07-16 22:55:20 +0300

    исправил перенаправление на страницу с авторизацией, когда пользователь обновляет сайт на странице с добавлением ревью

--- project/src/components/private-route.tsx
+++ project/src/components/private-route.tsx
@@ -2,6 +2,7 @@ import { AppRoute } from '../consts/routes';
 import { AuthorizationStatus } from '../consts/authhorization-status';
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../hooks';
+import { Loader } from './loader';
 
 type TPrivateRouteProps = {
   children: JSX.Element;
@@ -9,6 +10,10 @@ type TPrivateRouteProps = {
 
 export function PrivateRoute({ children }: TPrivateRouteProps): JSX.Element {
   const authStatus = useAppSelector((state) => state.authStatus);
+
+  if (authStatus === AuthorizationStatus.Unknown) {
+    return <Loader />;
+  }
 
   return authStatus === AuthorizationStatus.Auth ? (
     children

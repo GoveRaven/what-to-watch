da381943c6c227b470584f916589b21a7d5453d8
Даня <daniilevgen79@gmail.com>
2024-08-02 15:53:13 +0300

    настроил отправку комментария со страницы

--- project/src/components/review-form.tsx
+++ project/src/components/review-form.tsx
@@ -1,19 +1,32 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { RatingInput } from './rating-input';
+import { postComment } from '../store/api-action';
+import { useAppDispatch } from '../hooks';
+import { useParams } from 'react-router-dom';
 
 export function ReviewForm(): JSX.Element {
   const [text, setText] = useState('');
   const [rating, setRating] = useState('');
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
+  const filmId = Number(id);
 
   function handlerInputChange(chosenRating: string) {
     setRating(chosenRating);
+  }
+
+  function handelFormSumbit(event: FormEvent) {
+    event.preventDefault();
+    dispatch(
+      postComment({ comment: text, rating: Number(rating), id: filmId })
+    );
   }
 
   const isButtonDisabled = text.length < 50 || text.length > 400 || !rating;
 
   return (
     <div className="add-review">
-      <form action="#" className="add-review__form">
+      <form action="#" className="add-review__form" onSubmit={handelFormSumbit}>
         <div className="rating">
           <RatingInput rating={rating} onInputChange={handlerInputChange} />
         </div>

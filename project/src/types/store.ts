06b164a19f53baf158d25802b5cf53cb032dca0d
Даня <daniilevgen79@gmail.com>
2024-08-06 22:24:59 +0300

    добавил типы для слайсов

--- project/src/types/store.ts
+++ project/src/types/store.ts
@@ -1,5 +1,32 @@
+import { AuthorizationStatus } from '../consts/authhorization-status';
 import { store } from '../store';
+import { TFilm } from './films';
+import { TReview } from './reviews';
+import { TUser } from './user';
 
 export type TState = ReturnType<typeof store.getState>;
 
 export type TAppDispatch = typeof store.dispatch;
+
+export type TUserSlice = {
+  authStatus: AuthorizationStatus;
+  isAuthStatusChecked: boolean;
+  user: null | TUser;
+};
+
+export type TFilmsSlice = {
+  genre: string;
+  filmsByCurrentGenre: TFilm[];
+};
+
+export type TDataSlice = {
+  promoFilm: TFilm | null;
+  isPromoLoading: boolean;
+  allFilms: TFilm[];
+  areFilmsLoading: boolean;
+  chosenFilm: TFilm | null;
+  isFilmLoading: boolean;
+  similarFilms: TFilm[];
+  filmComments: TReview[];
+  isFilmCommentLoading: boolean;
+};

15bdfd75f3764db318480d23a7da3a5598a3137b
Даня <daniilevgen79@gmail.com>
2024-09-02 16:35:25 +0300

    добаивл тесты для middleware

---
+++ project/src/services/middleware.test.ts
@@ -0,0 +1,36 @@
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { AnyAction } from '@reduxjs/toolkit';
+import { redirect } from './middleware';
+import { TState } from '../types/store';
+import { redirectToRoute } from '../store/actions';
+import { AppRoute } from '../consts/routes';
+
+const fakeHistory = {
+  location: { pathname: '' },
+  push(path: string) {
+    this.location.pathname = path;
+  },
+};
+
+jest.mock('../components/history-routes/browser-history', () => fakeHistory);
+
+const middleware = [redirect];
+const mockStoreCreator = configureMockStore<TState, AnyAction>(middleware);
+const store = mockStoreCreator();
+
+describe('middleware: redirect', () => {
+  beforeEach(() => {
+    fakeHistory.push('');
+  });
+
+  it('should redirect to "/login" with redirectRouter action', () => {
+    store.dispatch(redirectToRoute(AppRoute.SignIn));
+    expect(fakeHistory.location.pathname).toBe(AppRoute.SignIn);
+    expect(store.getActions()).toEqual([redirectToRoute(AppRoute.SignIn)]);
+  });
+
+  it('should not to be redirect "/login" because bad acction', () => {
+    store.dispatch({ type: 'bad_action', payload: AppRoute.SignIn });
+    expect(fakeHistory.location.pathname).not.toBe(AppRoute.SignIn);
+  });
+});

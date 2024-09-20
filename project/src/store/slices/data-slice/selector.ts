1657e821d8f8b798313c44cc9210d487e3572951
Даня <daniilevgen79@gmail.com>
2024-09-20 20:19:43 +0300

    преттир прошёлся

--- project/src/store/slices/data-slice/selector.ts
+++ project/src/store/slices/data-slice/selector.ts
@@ -5,49 +5,38 @@ import { TState } from '../../../types/store';
 
 type TDataState = Pick<TState, SliceName.Data>;
 
-export const selectPromoFilm = (
-  state: TDataState
-): TFilm | null => state[SliceName.Data].promoFilm;
+export const selectPromoFilm = (state: TDataState): TFilm | null =>
+  state[SliceName.Data].promoFilm;
 
-export const selectIsPromoLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].isPromoLoading;
+export const selectIsPromoLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].isPromoLoading;
 
 export const selectAllFilms = (state: TDataState): TFilm[] =>
   state[SliceName.Data].allFilms;
 
-export const selectAreFilmsLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].areFilmsLoading;
+export const selectAreFilmsLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].areFilmsLoading;
 
-export const selectChosenFilm = (
-  state: TDataState
-): TFilm | null => state[SliceName.Data].chosenFilm;
+export const selectChosenFilm = (state: TDataState): TFilm | null =>
+  state[SliceName.Data].chosenFilm;
 
-export const selectIsFilmLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].isFilmLoading;
+export const selectIsFilmLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].isFilmLoading;
 
-export const selectSimilarFilms = (
-  state: TDataState
-): TFilm[] => state[SliceName.Data].similarFilms;
+export const selectSimilarFilms = (state: TDataState): TFilm[] =>
+  state[SliceName.Data].similarFilms;
 
-export const selectisSimilarFilmsLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].isSimilarFilmsLoading;
+export const selectareSimilarFilmsLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].areSimilarFilmsLoading;
 
-export const selectFilmComments = (
-  state: TDataState
-): TReview[] => state[SliceName.Data].filmComments;
+export const selectFilmComments = (state: TDataState): TReview[] =>
+  state[SliceName.Data].filmComments;
 
-export const selectAreFilmCommentsLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].areFilmCommentsLoading;
+export const selectAreFilmCommentsLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].areFilmCommentsLoading;
 
-export const selectFavoriteFilms = (
-  state: TDataState
-): TFilm[] => state[SliceName.Data].favoriteFilms;
+export const selectFavoriteFilms = (state: TDataState): TFilm[] =>
+  state[SliceName.Data].favoriteFilms;
 
-export const selectAreFavoriteFilmsLoading = (
-  state: TDataState
-): boolean => state[SliceName.Data].areFavoriteFilmsLoading;
+export const selectAreFavoriteFilmsLoading = (state: TDataState): boolean =>
+  state[SliceName.Data].areFavoriteFilmsLoading;

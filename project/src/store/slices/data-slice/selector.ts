0992ed11a9be14375f814f86bf89be2099d74900
Даня <daniilevgen79@gmail.com>
2024-09-20 20:09:30 +0300

    добавил тип

--- project/src/store/slices/data-slice/selector.ts
+++ project/src/store/slices/data-slice/selector.ts
@@ -3,49 +3,51 @@ import { TFilm } from '../../../types/films';
 import { TReview } from '../../../types/reviews';
 import { TState } from '../../../types/store';
 
+type TDataState = Pick<TState, SliceName.Data>;
+
 export const selectPromoFilm = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): TFilm | null => state[SliceName.Data].promoFilm;
 
 export const selectIsPromoLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].isPromoLoading;
 
-export const selectAllFilms = (state: Pick<TState, SliceName.Data>): TFilm[] =>
+export const selectAllFilms = (state: TDataState): TFilm[] =>
   state[SliceName.Data].allFilms;
 
 export const selectAreFilmsLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].areFilmsLoading;
 
 export const selectChosenFilm = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): TFilm | null => state[SliceName.Data].chosenFilm;
 
 export const selectIsFilmLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].isFilmLoading;
 
 export const selectSimilarFilms = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): TFilm[] => state[SliceName.Data].similarFilms;
 
 export const selectisSimilarFilmsLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].isSimilarFilmsLoading;
 
 export const selectFilmComments = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): TReview[] => state[SliceName.Data].filmComments;
 
 export const selectAreFilmCommentsLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].areFilmCommentsLoading;
 
 export const selectFavoriteFilms = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): TFilm[] => state[SliceName.Data].favoriteFilms;
 
 export const selectAreFavoriteFilmsLoading = (
-  state: Pick<TState, SliceName.Data>
+  state: TDataState
 ): boolean => state[SliceName.Data].areFavoriteFilmsLoading;

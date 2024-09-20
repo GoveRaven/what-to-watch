6d2c5619c2a7db26891c4a82ac23fce3aeeed41e
Даня <daniilevgen79@gmail.com>
2024-09-20 20:12:51 +0300

    добавил тип

--- project/src/store/slices/user-slice/selector.ts
+++ project/src/store/slices/user-slice/selector.ts
@@ -3,13 +3,15 @@ import { SliceName } from '../../../consts/store-action';
 import { TState } from '../../../types/store';
 import { TUser } from '../../../types/user';
 
+type TUserState = Pick<TState, SliceName.User>;
+
 export const selectAuthStatus = (
-  state: Pick<TState, SliceName.User>
+  state: TUserState
 ): AuthorizationStatus => state[SliceName.User].authStatus;
 
 export const selectIsAuthStatusChecked = (
-  state: Pick<TState, SliceName.User>
+  state: TUserState
 ): boolean => state[SliceName.User].isAuthStatusChecked;
 
-export const selectUser = (state: Pick<TState, SliceName.User>): TUser | null =>
+export const selectUser = (state: TUserState): TUser | null =>
   state[SliceName.User].user;
